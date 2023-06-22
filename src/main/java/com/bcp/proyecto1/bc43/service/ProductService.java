baa3399f05192865bbfed5a6091f165f652aa11d
Condori <condoril@royalsystems.net>
2023-06-22 18:12:43 -0500

    ultimo cambio para entregar

--- src/main/java/com/bcp/proyecto1/bc43/service/ProductService.java
+++ src/main/java/com/bcp/proyecto1/bc43/service/ProductService.java
@@ -7,9 +7,12 @@ import com.bcp.proyecto1.bc43.model.ProductType;
 import com.bcp.proyecto1.bc43.repository.ClientRepository;
 import com.bcp.proyecto1.bc43.repository.ProductRepository;
 import io.reactivex.rxjava3.core.*;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class ProductService {
@@ -26,48 +29,39 @@ public class ProductService {
         return Observable.fromPublisher(productRepository.findAll());
     }
 
-    public Observable<Product> createProduct(Product product) {
+    /*public Observable<Product> createProduct(Product product) {
         return Observable.create(emitter -> {
-            // Obtener el cliente asociado al producto
-            Client client = clientService.getClientById(product.getIdClient()).blockingFirst();
-            ProductType[] types = {ProductType.AHORRO,ProductType.CUENTA_CORRIENTE,ProductType.PLAZO_FIJO}
-            if (client != null && productRepository.countByClientIdAndTypeContains(product.getIdClient(), types) > 0) {
-                emitter.onError(new IllegalArgumentException("El cliente ya tiene una cuenta de ahorro, cuenta corriente o cuenta a plazo fijo."));
-            } else {
-                System.out.println(client);
-                productRepository.save(product)
-                        .doOnSuccess(savedProduct -> emitter.onComplete())
-                        .doOnError(error -> emitter.onError(error))
-                        .subscribe();
-            }
+            productRepository.save(product)
+                    .doOnSuccess(savedClient -> emitter.onComplete())
+                    .doOnError(error -> emitter.onError(error))
+                    .subscribe();
         });
+    }*/
+
+    public Observable<Product> createProduct(Product product) {
+        return clientService.getClientById(product.getIdClient())
+                .flatMap(client -> validateClientAndProduct(client, product).toObservable())
+                .flatMap(validatedProduct -> saveProduct(validatedProduct));
     }
 
-    public Completable validClientPersonal(Product product) {
-        ProductType[] types = {ProductType.AHORRO, ProductType.CUENTA_CORRIENTE, ProductType.PLAZO_FIJO};
+    private Single<Product> validateClientAndProduct(Client client, Product product) {
+        if (client.getType() == ClientType.PERSONAL) {
+            ProductType[] types = {ProductType.AHORRO, ProductType.CUENTA_CORRIENTE, ProductType.PLAZO_FIJO};
+            return productRepository.countByClientIdAndTypeContains(product.getIdClient(), types)
+                    .flatMap(count -> {
+                        if (count > 0) {
+                            return Single.error(new IllegalArgumentException("El cliente personal ya tiene una cuenta de ahorro, cuenta corriente o cuenta a plazo fijo."));
+                        } else {
+                            return Single.just(product);
+                        }
+                    });
+        } else {
+            return Single.just(product);
+        }
+    }
 
-        return clientService.getClientById(product.getIdClient())
-                .flatMapCompletable(existingClient -> {
-                    if (existingClient.getType() != ClientType.PERSONAL) {
-                        String errorMessage = "Client is not of type PERSONAL";
-                        return Completable.error(new IllegalArgumentException(errorMessage));
-                    } else {
-                        return productRepository.countByClientIdAndTypeContains(product.getIdClient(), types)
-                                .flatMapCompletable(count -> {
-                                    if (count > 0) {
-                                        String errorMessage = "Exceeded the maximum number of products for the client";
-                                        return Completable.error(new IllegalArgumentException(errorMessage));
-                                    } else {
-                                        System.out.println("guardar");
-                                        return Completable.fromPublisher(productRepository.save(product));
-                                    }
-                                });
-                    }
-                })
-                .onErrorResumeNext(error -> {
-                    String errorMessage = "An error occurred while validating the client";
-                    return Completable.error(new Exception(errorMessage));
-                });
+    private Observable<Product> saveProduct(Product product) {
+        return Observable.fromPublisher(productRepository.save(product));
     }
 
     public Observable<Product> updateProduct(String id, Product updatedProduct) {
